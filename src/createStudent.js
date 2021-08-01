e19ef71936e0bd5218331539742e164caf6a2eae
Juan Formoso <trajano.juan@gmail.com>
2021-08-01 19:18:56 -0400

    Finalizando requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const phrase = {
+    name: name,
+    feedback: () => "Eita pessoa boa!",
+  };
+  return phrase;
+};
 
 module.exports = createStudent;
